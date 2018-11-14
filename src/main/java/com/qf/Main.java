cf4268d434c12849c1a57edf004eec46efa5890a
ruanhao <ruanhao@qq.com>
2018-11-15 00:54:39 +0800

    第一次架构搭建完成

---
+++ src/main/java/com/qf/Main.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @Author ryan
+ * @Time 2018/11/15 0:42
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+    }
+}
